3fbf79e9783f1584d101925836f1703c56ac4402
Hossein-29 <hb3238816@gmail.com>
2026-02-05 18:16:41 +0330

    Remove redundant codes form tsp

--- src/problems/tsp.py
+++ src/problems/tsp.py
@@ -4,8 +4,6 @@ Traveling Salesman Problem (TSP) implementation.
 Test instances: n = 10, 20, 30, 50, 100 cities
 """
 
-import numpy as np
-
 
 class TSPProblem:
     """Traveling Salesman Problem."""
@@ -24,37 +22,4 @@ class TSPProblem:
         self.n_cities = problem_info.get("n_cities", 10)
         self.distance_matrix = problem_info.get("distance_matrix")
         self.known_optimal = problem_info.get("known_optimal")
-        
-    def generate_random_instance(self, n_cities: int, seed: int = None) -> np.ndarray:
-        """
-        Generate a random TSP instance.
-        
-        Args:
-            n_cities: Number of cities.
-            seed: Random seed for reproducibility.
-            
-        Returns:
-            Distance matrix.
-        """
-        # TODO: Implement random instance generation
-        # 1. Generate random city coordinates
-        # 2. Compute Euclidean distance matrix
-        raise NotImplementedError("generate_random_instance not yet implemented")
-    
-    def evaluate(self, tour: list) -> float:
-        """
-        Evaluate the total distance of a tour.
-        
-        Args:
-            tour: List of city indices representing the tour.
-            
-        Returns:
-            Total tour distance.
-        """
-        # TODO: Implement tour evaluation
-        raise NotImplementedError("evaluate not yet implemented")
-    
-    def get_distance_matrix(self) -> np.ndarray:
-        """Get the distance matrix for this instance."""
-        # TODO: Return or generate distance matrix
-        raise NotImplementedError("get_distance_matrix not yet implemented")
+
