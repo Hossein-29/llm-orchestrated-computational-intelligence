130cf006db62b4ffafce3982ce7a5c02938d9e9c
Aria Doustkhah <aria.vajarii@gmail.com>
2026-02-07 18:10:39 +0330

    implement load data for clustering problem

--- src/datasets/dataset_loader.py
+++ src/datasets/dataset_loader.py
@@ -1,16 +1,17 @@
 import numpy as np
 from scipy.spatial.distance import cdist
+import pandas as pd
+from sklearn.datasets import make_blobs
 
-DATASET_NAMES=["eil51","berlin52","kroa100","iris"]
+DATASET_NAMES=["eil51","berlin52","kroa100","iris","mall customers"]
 
 def dataset_loader(problem_info:dict):
-  if problem_info["content"]["dataset_name"].lower() in DATASET_NAMES:
 
-    if problem_info["problem_type"].lower()=="tsp":
-      return tsp_dataset_loader(problem_info)
-    
-    elif problem_info["problem_type"].lower()=="optimization":
-      return optimization_dataset_loader(problem_info)
+  if problem_info["problem_type"].lower()=="tsp":
+    return tsp_dataset_loader(problem_info)
+  
+  elif problem_info["problem_type"].lower()=="clustering":
+    return clustering_dataset_loader(problem_info)
 
 def tsp_dataset_loader(problem_info:dict):
 
@@ -36,5 +37,43 @@ def tsp_dataset_loader(problem_info:dict):
   problem_info["content"]["distance_matrix"]=distance_matrix
   problem_info["content"]["n_cities"]=n_cities
 
-def optimization_dataset_loader(problem_content:dict):
-  pass
+
+def clustering_dataset_loader(problem_info:dict):
+  source = problem_info["content"]["data_source"]
+
+  try:
+    if source.lower() in DATASET_NAMES:
+        if source == "iris":
+          df = pd.read_csv("src/problems/datasets/iris.csv")
+          return df
+        
+        elif source == "mall customer":
+          df = pd.read_csv("src/problems/datasets/Mall_Customers.csv")
+          return df
+        
+        else:
+            return pd.read_csv(f"src/problems/datasets/{source.lower()}.csv")
+        
+    elif source.lower() == "generated":
+      
+      n_samples=problem_info["content"]["n_samples"] if problem_info["content"]["n_samples"] != "not_specified" else 100,
+      n_clusters=problem_info["content"]["n_clusters"] if problem_info["content"]["n_clusters"] != "not_specified" else 2,
+      n_features=problem_info["content"]["n_features"] if problem_info["content"]["n_features"] != "not_specified" else 2,
+      cluster_std=problem_info["content"]["cluster_std"] if problem_info["content"]["cluster_std"] != "not_specified" else 1
+
+      X, y = make_blobs(
+          n_samples=n_samples,
+          centers=n_clusters,
+          n_features=n_features,
+          cluster_std=cluster_std,
+          random_state=42
+      )
+      columns = [f"Feature_{i+1}" for i in range(n_features)]
+      df = pd.DataFrame(X, columns=columns)
+
+      df["target"] = y
+      
+      return df
+        
+  except:
+    return pd.DataFrame(source)
